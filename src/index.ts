5d88f335acb39aa9dd9de8a52f0d62f840692647
Arnaud Renaud <arnaud.renaud@gmail.com>
2024-03-12 14:32:27 +0100

    Send select query result to client

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,16 @@ const dbClient = new pg.Client(
 );
 dbClient.connect();
 
+dbClient.query(`CREATE TABLE IF NOT EXISTS "Post" (
+  id SERIAL,
+  content TEXT NOT NULL
+);`);
+dbClient.query(`INSERT INTO "Post" ("content") VALUES ('Mon premier post.');`);
+
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.send("Hello world!");
+app.get("/", async (request, response) => {
+  return response.send(await dbClient.query(`SELECT * FROM "Post";`));
 });
 
 app.listen(3000, () => {
