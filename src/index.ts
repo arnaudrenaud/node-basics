f05bba961492cafe91d4f1c3938d3491682d07b8
Arnaud Renaud <arnaud.renaud@gmail.com>
2024-03-22 14:20:54 +0100

    Make sure server is connected to database before making requests

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,37 @@
 import express from "express";
 import pg from "pg";
 
-const dbClient = new pg.Client(process.env.DATABASE_URL);
-dbClient.connect();
+async function main() {
+  const dbClient = new pg.Client(process.env.DATABASE_URL);
+  await dbClient.connect();
 
-dbClient.query(`CREATE TABLE IF NOT EXISTS "Post" (
-  id SERIAL,
-  content TEXT NOT NULL
-);`);
-dbClient.query(`INSERT INTO "Post" ("content") VALUES ('Mon premier post.');`);
-
-const app = express();
-
-app.get("/", async (request, response) => {
-  const dbResponse = await dbClient.query(
-    `SELECT content FROM "Post" ORDER BY id DESC LIMIT 1;`
-  );
-  return response.send(dbResponse.rows[0].content);
-});
-
-app.post("/", async (request, response) => {
+  await dbClient.query(`CREATE TABLE IF NOT EXISTS "Post" (
+    id SERIAL,
+    content TEXT NOT NULL
+  );`);
   await dbClient.query(
-    `INSERT INTO "Post" ("content") VALUES ('Un nouveau post.');`
+    `INSERT INTO "Post" ("content") VALUES ('Mon premier post.');`
   );
-  return response.send("Post créé.");
-});
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000.");
-});
+  const app = express();
+
+  app.get("/", async (request, response) => {
+    const dbResponse = await dbClient.query(
+      `SELECT content FROM "Post" ORDER BY id DESC LIMIT 1;`
+    );
+    return response.send(dbResponse.rows[0].content);
+  });
+
+  app.post("/", async (request, response) => {
+    await dbClient.query(
+      `INSERT INTO "Post" ("content") VALUES ('Un nouveau post.');`
+    );
+    return response.send("Post créé.");
+  });
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000.");
+  });
+}
+
+main();
